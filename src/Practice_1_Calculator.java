8613b58ed552addba8d09c7d7c2c7fb81d68b035
meyup <m.eypoglu@gmail.com>
2024-04-25 11:02:05 +0300

    added comments

--- src/Practice_1_Calculator.java
+++ src/Practice_1_Calculator.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Practice_1_Calculator {
     public static void main(String[] args) {
+        // Kullanıcıdan sayı girdisi alındı
         Scanner input = new Scanner(System.in);
         System.out.print("Birinci sayıyı giriniz: ");
         double num1 = input.nextDouble();
         System.out.print("İkinci sayıyı giriniz: ");
         double num2 = input.nextDouble();
+        // Yapılacak işlem için girdi alındı
         System.out.print("Yapmak istediğiniz işleme karşılık gelen sayıyı seçiniz" +
                 "\n1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\n");
         int op = input.nextInt();
-
+        // Kullanıcı tarafından seçilen işleme göre switch ile farklı durumlar tanımlandı ve sonuç ekrana verildi
         switch (op) {
             case 1:
                 System.out.println("Toplamanın sonucu: " + (num1 + num2));
